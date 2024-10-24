73c9ed88a62e6ea7d549d91ee2d681aad10537f2
gnkz <gnkz.dev@gmail.com>
2024-10-23 23:18:33 -0300

    update tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -2,5 +2,11 @@ import { Token } from "@cryptoalgebra/sdk";
 import { DEFAULT_CHAIN_ID } from "./default-chain-id";
 
 export const STABLECOINS = {
-    USDT: new Token(DEFAULT_CHAIN_ID, '0x7d98346b3b000c55904918e3d9e2fc3f94683b01', 6, 'USDT', 'USDT')
-}
+  USDT: new Token(
+    DEFAULT_CHAIN_ID,
+    "0xD8D878Cbb4168B791d0EF57Da540d4FbAaF2Db41",
+    18,
+    "USDT",
+    "USDT"
+  ),
+};
